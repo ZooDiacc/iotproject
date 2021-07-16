1f0e7db0b966ac02f8939310f2938b629df8c20f
ZooDiacc <e_lyas@hotmail.fr>
2021-07-16 16:17:35 +0200

    syntax error

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-     lastname: {
+     lastName: {
         type: String,
         required: true
     },
